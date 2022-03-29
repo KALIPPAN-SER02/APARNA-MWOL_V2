710aa989a5f429233941f7a059e9c50e96556062
KALIPPAN-SER02 <102555115+KALIPPAN-SER02@users.noreply.github.com>
2022-03-29 16:21:18 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,16 +6,16 @@ if (Config.WORKTYPE == 'private') {
 
     Julie.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '918281370025-1634557305@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Toxic-Alexa created by Turbo*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Aparna-mwol_v2 created by Kalippanser*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n Toxic-Alexa created by Turbo', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n Aparna-mwol_v2 created by Kalippanser', MessageType.text);
         }
     }));
 }
@@ -24,16 +24,16 @@ else if (Config.WORKTYPE == 'public') {
 
     Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '918281370025-1634557305@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Toxic-Alexa created by Turbo*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Aparna-mwol_v2 created by Kalippanser*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n Toxic-Alexa created by Turbo', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n Aparna-mwol_v2 created by Kalippanser', MessageType.text);
         }
     }));
 }
